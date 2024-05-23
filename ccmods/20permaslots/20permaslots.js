41bd4c080334c1c1c7c07f720b1a33602f113291
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2024-05-23 19:14:19 -0400

    Create 20permaslots.js

---
+++ ccmods/20permaslots/20permaslots.js
@@ -0,0 +1,15 @@
+const numerals = [,,,,'V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX'];
+const desc = loc("Placing an upgrade in this slot will make its effects <b>permanent</b> across all playthroughs.");
+for (let i = 6; i <= 20; i++) {
+  new Game.Upgrade(`Permanent upgrade slot ${numerals[i-1]}`,desc,Math.pow(100,i)*i,[4,10]);
+  Game.last.parents=[Game.Upgrades[`Permanent upgrade slot ${numerals[i-2]}`]];
+  Game.last.iconFunction = () => Game.PermanentSlotIcon(i-1).join(' ')==[i-1,10].join(' ')?[4,10]:Game.PermanentSlotIcon(i-1);
+  Game.last.activateFunction = () => Game.AssignPermanentSlot(i-1);
+  Game.last.posX = 293.266*Math.sin(Math.pow(i,0.709)+15856.6)+356.679;
+  Game.last.posY = -163.645*Math.cos(Math.pow(i,0.777)+0.635)+120.224+(-6.755*Math.pow(i,2));
+  Game.last.pool = 'prestige';
+  Game.PrestigeUpgrades.push(Game.last);
+  Game.permanentUpgrades.push(-1);
+}
+LocalizeUpgradesAndAchievs();
+Game.BuildAscendTree();
