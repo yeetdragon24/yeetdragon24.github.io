da57d1a43e5ad830855b97ee64d820ba55dc15fe
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2024-06-23 11:03:37 -0400

    Create subwaysurfers.js

---
+++ ccmods/subwaysurfers/subwaysurfers.js
@@ -0,0 +1,11 @@
+Game.registerMod('subway suffers',{
+    init:function() {
+        l('commentsText').innerHTML='<iframe width="250" height="150" src="https://www.youtube.com/embed/e9LAy6rfoVY?loop=1&autoplay=1&playlist=e9LAy6rfoVY" style="margin-top:-30px"></iframe>';
+    },
+    save:function() {
+        return 'why tf are you using this mod';
+    },
+    load:function() {
+        Game.Notify('what','you are not getting sigmagintillions with your gyattzamok sells and rizzerect abomination confirms',[123,123],200);
+    }
+});
